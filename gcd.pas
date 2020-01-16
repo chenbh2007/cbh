ae562ef82b4db011f8be9d409eb9fe15dcaab9ad
chenbh2007 <56187712+chenbh2007@users.noreply.github.com>
2020-01-16 15:07:35 +0800

    Update gcd.pas

--- gcd.pas
+++ gcd.pas
@@ -1,12 +1,5 @@
-var
-  a,b:longint;
 function gcd(n,m:longint):longint;
 begin
   if n mod m=0 then exit(m);
   exit(gcd(m,n mod m));
 end;
-begin
-  readln(a,b);
-  if f(a,b)=1 then writeln('Yes')
-    else writeln('No');
-end.
